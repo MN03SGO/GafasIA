f54df1fd5860d50ae71c07ff68005b20db70c8e1
MN03SGO <anonimo200703@gmail.com>
2025-10-30 00:50:36 -0600

    Conexion con Flask y transmisión de video

---
+++ server/app.py
@@ -0,0 +1,58 @@
+from flask import Flask, Response, render_template, stream_with_context
+import time
+import atexit
+import sys
+import os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = os.path.dirname(script_dir)
+sys.path.append(root_dir)
+
+from ejemplo_ocr_integrado import GafasIACompleto
+print("flask")
+template_folder_path = os.path.join(root_dir, 'templates')
+app = Flask(__name__, template_folder=template_folder_path)
+
+print("instancia global")
+gafas = GafasIACompleto()
+gafas.ejecutando = True #bandera para el bucle del generador
+
+@stream_with_context #generador de video web
+def generador_web():
+    try:
+        if not gafas.picam2 or not gafas.picam2.started:
+            if not gafas.iniciar_camara():
+                print(" No se pudo iniciar la cámara para el generador web.")
+                return
+        for frame_bytes in gafas.generar_frames_flask():
+            yield frame_bytes
+            
+    except Exception as e:
+        print(f"Error en el generador web de Flask: {e}")
+    finally:
+        print("El stream de video se ha detenido.")
+        # espera, para poder reconectar
+
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
+@app.route('/video_feed')
+def video_feed():
+    print("Solicitud recibida para /video_feed")
+    return Response(
+        generador_web(),
+        mimetype='multipart/x-mixed-replace; boundary=frame'
+    )
+
+
+def limpiar_al_salir():
+    print("Detectado cierre del servidor Flask. Limpiando recursos...")
+    gafas.ejecutando = False
+    gafas._limpiar_recursos()
+
+atexit.register(limpiar_al_salir)
+
+if __name__ == '__main__':
+    print("Iniciando servidor Flask...")
+    app.run(host='0.0.0.0', port=5000, debug=False, threaded=True)
